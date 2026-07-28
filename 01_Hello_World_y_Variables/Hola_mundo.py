9b1e072ba335b5b3eaa4c31a69f9b51a19b9fb5c
Soy-SantiagoDev <santimarinsito@gmail.com>
2026-07-28 03:12:17 -0500

    Update Hola_mundo.py

--- 01_Hello_World_y_Variables/Hola_mundo.py
+++ 01_Hello_World_y_Variables/Hola_mundo.py
@@ -1,2 +1,10 @@
-# Imprime un mensaje en la consola.
-print("HOLA MUNDO")
+# -----------------------------------------
+# Hola Mundo
+# -----------------------------------------
+# Este es el primer programa que todo
+# programador suele escribir.
+# La función print() permite mostrar
+# información en la consola.
+# -----------------------------------------
+
+print("¡Hola Mundo!")
