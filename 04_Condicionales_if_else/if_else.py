9e06df8c42a2e9ac58aa8a113aedf1e2a67be4ba
Soy-SantiagoDev <santimarinsito@gmail.com>
2026-07-28 03:48:42 -0500

    Update if_else.py

--- 04_Condicionales_if_else/if_else.py
+++ 04_Condicionales_if_else/if_else.py
@@ -1,11 +1,18 @@
-# int() convierte el dato ingresado por el usuario de texto a un número entero.
+# =====================================
+# Condicionales if y else
+# =====================================
+# Los condicionales permiten que un
+# programa tome decisiones dependiendo
+# de una condición.
+# =====================================
+
+# Solicitar la edad del usuario.
+
 edad = int(input("¿Cuál es tu edad? "))
 
-# if evalúa una condición. Si la condición es verdadera,
-# se ejecuta el bloque de código que está debajo.
+# Comprobar si la persona es mayor de edad.
+
 if edad >= 18:
-    # Se ejecuta cuando la edad es 18 o mayor.
     print("Eres mayor de edad.")
 else:
-    # else se ejecuta cuando la condición del if es falsa.
     print("Eres menor de edad.")
