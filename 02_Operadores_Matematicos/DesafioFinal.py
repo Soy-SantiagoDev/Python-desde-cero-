66f02d594b44c96e28e0affb62b50d8f83cb7c30
Soy-SantiagoDev <santimarinsito@gmail.com>
2026-07-28 03:34:56 -0500

    Add files via upload

---
+++ 02_Operadores_Matematicos/DesafioFinal.py
@@ -0,0 +1,21 @@
+# =====================================
+# DESAFÍO FINAL
+# =====================================
+
+# En una tienda:
+
+# Cada cuaderno cuesta 4 dólares.
+# Cada lápiz cuesta 2 dólares.
+
+# Una persona compra:
+
+# 5 cuadernos
+# 8 lápices
+
+# Calcula:
+
+# 1. El costo de los cuadernos.
+# 2. El costo de los lápices.
+# 3. El total a pagar.
+
+# Utiliza variables.
