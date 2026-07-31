be592c5654ef178b09c8c25bd6fcdb49536deb66
Soy-SantiagoDev <santimarinsito@gmail.com>
2026-07-30 20:48:07 -0500

    Update elif.py

--- 09_condicional_elif/elif.py
+++ 09_condicional_elif/elif.py
@@ -1,8 +1,24 @@
-nota = int(input("Ingresa tu calificacion de (0 - 100)"))
+# =====================================
+# Condicional ELIF
+# =====================================
+# La estructura elif permite evaluar
+# varias condiciones en un mismo programa.
+#
+# Python ejecutará únicamente el primer
+# bloque cuya condición sea verdadera.
+# =====================================
+
+# Solicitar la calificación del estudiante.
+
+nota = int(input("Ingresa tu calificación (0 - 100): "))
+
+# Evaluar la calificación.
 
 if nota >= 90:
     print("Excelente")
-elif nota >=70:
+
+elif nota >= 70:
     print("Aprobado")
+
 else:
-    print("Reprobastes")
+    print("Reprobaste")
