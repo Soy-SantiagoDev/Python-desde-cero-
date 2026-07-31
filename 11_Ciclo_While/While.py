2637ab5aaa3967d6044f86987eef2c18ae1c9135
Soy-SantiagoDev <santimarinsito@gmail.com>
2026-07-30 21:23:41 -0500

    Update While.py

--- 11_Ciclo_While/While.py
+++ 11_Ciclo_While/While.py
@@ -1,5 +1,25 @@
+# =====================================
+# Ciclo WHILE
+# =====================================
+# El ciclo while permite repetir un
+# bloque de código mientras una condición
+# sea verdadera.
+# =====================================
+
+# Crear un contador.
+
 contador = 1
-while contador<= 10:
-    print(f"Repeticion numero  {contador}")
+
+# Repetir mientras el contador
+# sea menor o igual a 10.
+
+while contador <= 10:
+    print(f"Repetición número {contador}")
+
+    # Aumentar el contador en 1.
     contador += 1
+
+# Este mensaje se ejecuta cuando
+# el ciclo termina.
+
 print("Fin del programa.")
